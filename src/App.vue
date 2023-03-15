ca2c86811e3f46896db6810df2f004e509c8cbca
Ludovic Fourcroy <ludovic.fourcroy@willemse.com>
2023-03-15 18:02:13 +0100

    v-for log

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,17 @@
 import { ref } from 'vue'
 
 const header = ref('Shopping List App')
+const items = ref([
+  { id: 1, label: "Milk" },
+  { id: 2, label: "Bread" },
+  { id: 3, label: "Eggs" },
+  { id: 4, label: "Cheese" }
+])  
 </script>
 
 <template>
   <h1>{{ header }}</h1>
+  <ul>
+    <li v-for="({ id, label }, index) in items" :key="id"> {{ index }} {{ label }}</li>
+  </ul>
 </template>
