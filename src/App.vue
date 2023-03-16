d410de9850fe528a4278fe29b2e4464dc75e5eb4
unknown <ludovicfourcroy8@gmail.com>
2023-03-16 16:27:19 +0100

    add lib to test drag

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import Drag from './components/Drag.vue';
+import draggable from "vuedraggable";
+
+
+const drag = ref(false);
 
 const header = ref('Shopping List App')
 const items = ref([
@@ -8,11 +13,44 @@ const items = ref([
   { id: 3, label: "Eggs" },
   { id: 4, label: "Cheese" }
 ])  
+
 </script>
 
 <template>
+
+  <Drag />
+  <FormKit
+  type="range"
+  name="strength"
+  id="strength"
+  label="Strength"
+  value="5"
+  validation="min:2|max:9"
+  validation-visibility="live"
+  min="1"
+  max="10"
+  step="1"
+  help="How many strength points should this character have?"
+  />
+
   <h1>{{ header }}</h1>
+  <div>
+    <draggable
+      v-model="items"
+      @start="drag = true"
+      @end="drag = false"
+      item-key="id"
+    >
+      <template #item="{ element }">
+        <div>{{ element }}</div>
+      </template>
+    </draggable>
+
+  </div>
   <ul>
     <li v-for="({ id, label }, index) in items" :key="id"> {{ index }} {{ label }}</li>
   </ul>
+
+
+  
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
+
+import { plugin, defaultConfig } from '@formkit/vue'
+
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App).use(plugin, defaultConfig).mount('#app')
